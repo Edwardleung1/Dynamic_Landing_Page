dbbb186c30756b99bdc9647df8e8b7d1d2ea9d69
Edward Leung <edward_leung_12@hotmail.com>
2020-07-30 14:15:32 +0100

    added zero function and bg/greeting

--- js/main.js
+++ js/main.js
@@ -19,5 +19,42 @@ function showTime() {
   hour = hour % 12 || 12; // Instead of 13 it will be 1pm
 
   // Output Time
-  time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`; // Template literal
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )}`; // Template literal
+
+  // Call this showTime every second
+  setTimeout(showTime, 1000);
 }
+
+// Add Zeros to time
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+    greeting.textContent = "Good Morning";
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    greeting.textContent = "Good Afternoon";
+  } else {
+    // Evening
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = "Good Evening";
+    document.body.style.color = "white";
+  }
+}
+
+// Run
+showTime();
